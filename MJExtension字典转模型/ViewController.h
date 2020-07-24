991b51efe6f86cbd321065620619e2e61df25211
Jobs <>
2020-07-24 10:22:43 +0700

    first commit

---
+++ MJExtension字典转模型/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  MJExtension字典转模型
+//
+//  Created by wanglei on 16/12/29.
+//  Copyright © 2016年 wanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MJExtension.h"
+#import "CZStatus.h"
+#import "CZAd.h"
+#import "CZWeibo.h"
+#import "CZUser.h"
+#import "User.h"
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
